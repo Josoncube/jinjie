2865e8ae11b6910554cf8531b6b7240139c2ce38
Josoncube <1206860502@qq.com>
2020-03-31 15:04:19 +0800

    修改

--- src/Helloworld.java
+++ src/Helloworld.java
@@ -1,7 +1,7 @@
 
 public class Helloworld {
 	public static void main(String[] age){
-//git测试
+//git测试一下
 		System.out.println("----欢迎进入欣知商城----");
 		System.out.println("主菜单>购买商品");
 		System.out.println("1.耐克T恤衫");
