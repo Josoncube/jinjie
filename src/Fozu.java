8585fe117b94a36b4612431be431abb601d64654
Josoncube <1206860502@qq.com>
2020-03-31 14:43:59 +0800

    测试上传

---
+++ src/Fozu.java
@@ -0,0 +1,26 @@
+
+public class Fozu {
+	public static void main(String[] args) {
+		System.out.println("................∞¢√÷Õ”∑................");
+		System.out.println("\t\t _oo0oo_\t\t");
+		System.out.println("\t\to8888888o\t\t");
+		System.out.println("\t\t88\" . \"88\t\t");
+		System.out.println("\t\t(| -_- |)\t\t");
+		System.out.println("\t\t0\\  =  /0\t\t");
+		System.out.println("\t      __/ '---' \\__\t");
+		System.out.println("\t    .' \\|\t|/'.\t");
+		System.out.println("\t   / \\\\|||  :  |||// \\\t");
+		System.out.println("\t  / _||||| -Öd-|||||_ \\\t");
+		System.out.println("\t |   | \\\\\\  -  /// |   |\t");
+		System.out.println("\t | \\_|  ''\\---/''  |_/ |\t");
+		System.out.println("\t \\  .-\\__  '-'  __/-.  /\t");
+		System.out.println("\t__'. .'  /--.--\\  '. .'__\t");
+		System.out.println("      .\"\" '<  '.__\\_<|>_/__.' >' \"\"");
+		System.out.println("     ||:   '-\\ '.;'\\ _ /';.'/- '  : ||");
+		System.out.println("      \\ \\ '_.  \\_ __\\ / __ _/  .-'/ /");
+		System.out.println("  ==== '-.___ '.__ \\____/__.-' __.-' ====");
+		System.out.println("\t\t   '=---='\t\t");
+		System.out.println();
+		System.out.println("..............∑◊Ê±£””£¨”¿Œﬁbug..............");
+}
+}
---
+++ src/Helloworld.java
@@ -0,0 +1,13 @@
+
+public class Helloworld {
+	public static void main(String[] age){
+		System.out.println("----欢迎进入欣知商城----");
+		System.out.println("主菜单>购买商品");
+		System.out.println("1.耐克T恤衫");
+		System.out.println("2.阿迪达斯跑鞋");
+		System.out.println("3.杨树林香水");
+		System.out.println("4.兰博基尼跑车");
+		System.out.println("5.散发着恶臭的袜子");
+		System.out.println("----欢迎加入欣知商城----");
+	}	
+}
